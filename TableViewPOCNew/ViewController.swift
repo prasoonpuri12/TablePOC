f139b867cb8bacb5eca08224c6624428b1da2639
Prasoon Puri <prasoonpuri@administorsmini.cybage.com>
2015-11-04 12:28:58 +0530

    Initial Commit

---
+++ TableViewPOCNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPOCNew
+//
+//  Created by Prasoon Puri on 11/4/15.
+//  Copyright (c) 2015 Prasoon Puri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
